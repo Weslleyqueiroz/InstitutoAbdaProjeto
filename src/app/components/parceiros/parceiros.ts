4dae1c585715ef7327fa65f512d00711522de346
weslleyqueiroz <weslleyqueiroz773@gmail.com>
2026-04-20 11:51:54 -0300

    atualizando .gitignore para ignorar arquivos privados

--- src/app/components/parceiros/parceiros.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-parceiros',
-  imports: [],
-  templateUrl: './parceiros.html',
-  styleUrl: './parceiros.css',
-})
-export class Parceiros {}
